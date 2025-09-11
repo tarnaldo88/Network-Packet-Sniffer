c4a2428bb0358107b25639967f114bb6f2186d14
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-11 15:55:03 -0700

    made get mac address function to format the bytes and join them.

--- sniffer.py
+++ sniffer.py
@@ -6,3 +6,9 @@ import textwrap
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[:14]
+
+#return a formatted mac address
+def get_mac_addr(bytes_addr):    
+    bytes_str = map('{:02x}'.format, bytes_addr)
+    #joins the pieces made in map above, and then uppercases them
+    return ':'.join(bytes_addr).upper()
