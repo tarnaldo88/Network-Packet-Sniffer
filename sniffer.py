733bbea399f685003826753c6be5e417c4873996
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-11 17:34:14 -0700

    implemented tcp segment function, capturing the flags and bit manipulating to get each flag.

--- sniffer.py
+++ sniffer.py
@@ -53,11 +53,22 @@ def icmp_packet(data):
     icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
     return icmp_type, code, checksum, data[4:]
 
-#unpack TCP
-def tcp_packet(data):
-    
+#unpack TCP segment
+def tcp_segment(data):
+    (src_port, des_port, sequence, acknowledgement, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 2) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, des_port, sequence, acknowledgement, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
 
 #unpack UDP
 def udp_packet(data):
+    #placeholder
+    return 0
+
 
 main()
