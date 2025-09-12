d937dd6ce8ad375f4845dd0f19d73baf944c0eca
tarnaldo88 <tarnaldo88@gmail.com>
2025-09-11 17:39:11 -0700

    implemented udp packet func and format multi line func

--- sniffer.py
+++ sniffer.py
@@ -67,8 +67,17 @@ def tcp_segment(data):
 
 #unpack UDP
 def udp_packet(data):
-    #placeholder
-    return 0
+    src_port, dest_port, size =  struct.unpack('! H H 2x H', data[8:])
+    return src_port, dest_port, size, data[8:]
+
+#formats the multiple line data
+def format_multi_line(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+        if size % 2:
+            size -= 1
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
 
 
 main()
